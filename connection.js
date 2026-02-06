3cf88c6b8297f760ddc809a54c8d2a2bab3da55a
Tushar-Deve <tushartushar7090@gmail.com>
2026-02-06 13:13:59 +0530

    Fix DB env vars and server config for Railway

--- connection.js
+++ connection.js
@@ -2,13 +2,22 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//    port: process.env.DB_PORT
+// });
+
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-   port: process.env.DB_PORT
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
+
 
 db.connect((err) => {
   if (err) {
