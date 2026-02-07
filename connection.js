e3e419ed4f925c7261b0c8e84e11cfc61dd6e7ce
Tushar-Deve <tushartushar7090@gmail.com>
2026-02-07 19:28:03 +0530

    update

--- connection.js
+++ connection.js
@@ -1,47 +1,26 @@
-import mysql from "mysql2";
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const db = mysql.createPool({
+  host: process.env.MYSQLHOST.trim(),      // ✅ trim() se extra newline/space remove
+  user: process.env.MYSQLUSER.trim(),
+  password: process.env.MYSQLPASSWORD.trim(),
+  database: process.env.MYSQLDATABASE.trim(),
+  port: Number(process.env.MYSQLPORT),             // number hi rehne do
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.error("❌ Database connection failed:", err);
+    console.error("❌ DB pool connection failed:", err);
   } else {
-    console.log("✅ Connected to MySQL database");
-        conn.release();
+    console.log("✅ DB pool connected successfully");
+    connection.release(); // ✅ pool me release karna hota hai
   }
 });
 
 export default db;
-
-
-// import mysql from "mysql2";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const db = mysql.createPool({
-//   host: process.env.MYSQLHOST.trim(),      // ✅ trim() se extra newline/space remove
-//   user: process.env.MYSQLUSER.trim(),
-//   password: process.env.MYSQLPASSWORD.trim(),
-//   database: process.env.MYSQLDATABASE.trim(),
-//   port: process.env.MYSQLPORT,             // number hi rehne do
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
-// db.getConnection((err, connection) => {
-//   if (err) {
-//     console.error("❌ DB pool connection failed:", err);
-//   } else {
-//     console.log("✅ DB pool connected successfully");
-//     connection.release(); // ✅ pool me release karna hota hai
-//   }
-// });
-
-// export default db;
