58844dd0bf7b6cb05d8cb41101d3a19ce301a7cd
Tushar-Deve <tushartushar7090@gmail.com>
2026-02-07 18:52:50 +0530

    update

--- connection.js
+++ connection.js
@@ -18,3 +18,30 @@ db.connect((err) => {
 });
 
 export default db;
+
+
+// import mysql from "mysql2";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const db = mysql.createPool({
+//   host: process.env.MYSQLHOST.trim(),      // ✅ trim() se extra newline/space remove
+//   user: process.env.MYSQLUSER.trim(),
+//   password: process.env.MYSQLPASSWORD.trim(),
+//   database: process.env.MYSQLDATABASE.trim(),
+//   port: process.env.MYSQLPORT,             // number hi rehne do
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// db.getConnection((err, connection) => {
+//   if (err) {
+//     console.error("❌ DB pool connection failed:", err);
+//   } else {
+//     console.log("✅ DB pool connected successfully");
+//     connection.release(); // ✅ pool me release karna hota hai
+//   }
+// });
+
+// export default db;
