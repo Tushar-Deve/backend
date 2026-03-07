fd5be0633c8416b446778d6e7158506492f93429
Tushar-Deve <tushartushar7090@gmail.com>
2026-03-07 17:10:07 +0530

    Added all files

--- database/db.js
+++ database/db.js
@@ -1,4 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
+require("dotenv").config(); // ये .env पढ़ने के लिए जरूरी है
+
+const dbPath = process.env.DB_PATH || "./database/test_marks.db";
 
 const db = new sqlite3.Database("./database/test_marks.db", (err) => {
   if (err) {
